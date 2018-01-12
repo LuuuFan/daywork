72c818e0157d3ab758805150c3e87d10f1bd1d63
luuufan <angelia.fan@gmail.com>
2018-01-11 23:42:33 -0800

    finish courseLoad

--- W5D4/JS/phase_6_students_and_courses.js
+++ W5D4/JS/phase_6_students_and_courses.js
@@ -24,11 +24,16 @@ Student.prototype.courseLoad = function () {
   // console.log(hash.typeOf)
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG
-  this.courses.forEach( x => hash[x.dept] = x.credit);
+  // this.courses.forEach( x => hash[x.dept] = x.credit);
 // WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG WRONG
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   //console.log(x.credit));
   // console.log(hash);
+  this.courses.forEach( x => {
+    hash[x.dept] = hash[x.dept] || 0;
+    hash[x.dept] += x.credit;
+  });
+
   return hash;
 };
 
@@ -53,8 +58,22 @@ Course.prototype.addStudent = function (student) {
 Student.prototype.hasConflict = function () {
 };
 
-const lu = new Student('Lu', 'Fan');
-const ruby = new Course('Ruby', 'OOP', 10);
-lu.enroll(ruby);
-lu.courseLoad();
+// const lu = new Student('Lu', 'Fan');
+// const ruby = new Course('Ruby', 'OOP', 10);
+// lu.enroll(ruby);
+// lu.courseLoad();
 // console.log(lu.courseLoad().OOP);
+
+let student1 = new Student("Nigel", "Leffler");
+let course1 = new Course("101", "CS", 3, ["mon", "wed", "fri"], 1);
+let course2 = new Course("201", "CS", 3, ["wed"], 1);
+let course3 = new Course("301", "ENG", 3, ["tue"], 1);
+let course4 = new Course("401", "BIO", 3, ["mon", "wed", "fri"], 2);
+console.log(student1.name());
+student1.enroll(course1);
+student1.enroll(course3);
+student1.enroll(course2);
+console.log(student1.courseLoad());
+// console.log('should be true = ' + course1.conflictsWith(course2));
+// console.log('should be false = ' + course1.conflictsWith(course3));
+// console.log('should be false = ' + course1.conflictsWith(course4));
